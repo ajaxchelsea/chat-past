4b2c81d20a21101773e2f108e2d768c24102bce6
GL <ajaxchelsea@gmail.com>
2023-05-29 20:10:58 +0800

    replace default prompt and llm

--- asking.py
+++ asking.py
@@ -1,10 +1,10 @@
 import argparse
 import os
-
+from langchain import PromptTemplate
+from langchain.chat_models import ChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
 
 from helpers import print_documents
 
@@ -27,7 +27,19 @@ relevant_docs = vectordb.similarity_search(query, top_k)
 print_documents(relevant_docs)
 
 # Step 5：组成 context，向 OpenAI 发出真正请求
-chain = load_qa_chain(OpenAI(temperature=0.7, max_tokens=1024), chain_type="stuff")
+prompt_template = """
+First use the following context to answer the question at the end.
+Then you must provide your personal opinion based on other information you know, and start with "My Opinion: ".
+Use Chinese to answer unless the context is English.
+
+{context}
+
+Question: {question}
+Helpful Answer:
+"""
+
+combined_prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+chain = load_qa_chain(ChatOpenAI(model="gpt-3.5-turbo", temperature=0.7, max_tokens=1024), chain_type="stuff", prompt = combined_prompt)
 answer = chain.run(input_documents=relevant_docs, question=query)
 print("answer: ")
 print(answer)
